5ffbaefb6d6ea59e032190188b42bf63d08e9585
hhuyn019 <hhuyn019@ucr.edu>
2020-01-09 16:19:29 -0800

    Fifth commit

--- test.cpp
+++ test.cpp
@@ -7,7 +7,27 @@ TEST(EchoTest, HelloWorld) {
 	EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+	char* test_val[1]; test_val[0] = "./c-echo";
+	EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, Numbers) {
+	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "241435";
+	EXPECT_EQ("241435", echo(2,test_val));
+}
+
+TEST(EchoTest, SpecialChars) {
+	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "*&^%$#";
+	EXPECT_EQ("*&^%$#", echo(2,test_val));
+}
+
+TEST(EchoTest, Endline) {
+	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "\n";
+	EXPECT_EQ("\n", echo(2,test_val));
+}
+
 int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
 }
